92de574dba418af4db1025102adb10272e073cc4
Michael Darr <medarr@email.wm.edu>
2019-07-26 09:58:45 -0400

    custom select dropdown styling

--- src/components/FilterOptions.vue
+++ src/components/FilterOptions.vue
@@ -1,22 +1,25 @@
 <template>
     <div class="filter-options">
         <div class="artist-time-period">
-            <label for="time-period">Use Favorite Artists From</label>
-            <br>
-            <select
-                id="time-period"
-                v-model="selected"
-            >
-                <option value="short">
-                    This Month
-                </option>
-                <option value="medium">
-                    Last 6 Months
-                </option>
-                <option value="long">
-                    All Time
-                </option>
-            </select>
+            <div class="select-label-pair">
+                <label for="time-period">Using Your Favorite Artists From</label>
+                <div class="select">
+                    <select
+                        id="time-period"
+                        v-model="selected"
+                    >
+                        <option value="short">
+                            This Month
+                        </option>
+                        <option value="medium">
+                            The Last 6 Months
+                        </option>
+                        <option value="long">
+                            All Time
+                        </option>
+                    </select>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -43,5 +46,42 @@ export default class FilterOptions extends Vue {
 
 .artist-time-period {
     text-align: left;
+}
+
+.select-label-pair {
+    display: inline-flex;
+    flex-direction: column;
+}
+
+.select {
+    background: url(/select_arrow_right.png) no-repeat 96% 0;
+    height: 2em;
+    overflow: hidden;
+    width: 170px;
+    margin-top: 4px;
+}
+
+.select:hover {
+    background: url(/select_arrow_down.png) no-repeat 96% 0;
+}
+
+label {
+    font-size: 1em;
+}
+
+select {
+    background: transparent;
+    font-size: 1em;
+    width: 186px;
+    margin-left: -4px;
+    color: #b3b3b3;
+    border: 0px;
+    outline: 0px;
+}
+
+option {
+    font-size: 0.9em;
+    background-color: #111;
+    color: #000;
 }
 </style>
